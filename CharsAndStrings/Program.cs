5eb1a41b0fed929fb67bae9b608b4f8fadccebfb
salallegra <salvatore.allegra@smoothstack.com>
2022-09-30 14:09:20 -0700

    Pass a string to a function that contains or doesn't contain the characters needed to build a string "F-35" and check if the "F-35" can be built with the input string

--- CharsAndStrings/Program.cs
+++ CharsAndStrings/Program.cs
@@ -6,6 +6,11 @@
         {
             playWithCharacters();
             playWithStrings();
+
+            //Let's see if we can build the string "F-35" out of the characters in a string
+            string f35String = "axdi-95x34598fF36";
+            bool isF35ComboPossible = containsF35(f35String);
+            Console.WriteLine("Is an F-35 Possible? " + isF35ComboPossible);
             
         }
 
@@ -37,6 +42,45 @@
             //get the 3rd character in a string
             charInString = aString[2];
             Console.WriteLine(charInString);
+        }
+        static bool containsF35(string f35String)
+        {
+            //Need to check if there is an F, a -, a 3 and a 5 in the string.
+            //lets pseudocode it without code first
+            /* Input case "7yk" will return false, you can't build an F-35 out of that string
+             * Loop through stromg and Check first char, second char and 3rd char against an F - 3 5
+             * use select case to set boolean value F satisfied, - satisfied etc....
+             * use if statement to check if all boolean values are true
+             * return true or false accordingly
+             * loop through the string parameter
+             */
+            bool containsF = false;
+            bool containsHyphen = false;
+            bool contains3 = false;
+            bool contains5 = false;
+
+            foreach(char c in f35String)
+            {
+                switch (c)
+                {
+                    case 'F':
+                        containsF = true;
+                        break;
+                    case '-':
+                        containsHyphen = true;
+                        break;
+                    case '3':
+                        contains3 = true;
+                        break;
+                    case '5':
+                        contains5 = true;
+                        break;
+                }
+            }
+            if(containsF == true && containsHyphen == true && contains3 == true && contains5 == true)
+                return true;
+            else
+                return false;
         }
     }
 }
