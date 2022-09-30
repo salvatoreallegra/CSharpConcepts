f8fad9d373091370bd39d400ae293871776b5d5f
salallegra <salvatore.allegra@smoothstack.com>
2022-09-30 13:34:38 -0700

    Add some string and character operations

--- CharsAndStrings/Program.cs
+++ CharsAndStrings/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            playWithCharacters();
+            playWithStrings();
+            
+        }
+
+        static void playWithCharacters()
+        {
+            //Let's try some static char methods
+            char aCharacter = 'a';
+            //is it a digit?  True or False
+            Console.WriteLine(Char.IsDigit(aCharacter));
+
+            char aCharacter9 = '9';
+            //is it a digit?  True or False
+            Console.WriteLine(Char.IsDigit(aCharacter9));
+
+            //let's see if we can assign a unicode value to a char, should be capital 'A'
+            char unicodeChar = '\u0041';
+            Console.WriteLine(unicodeChar);
+
+            //Print out the next unicode char using the increment operator
+            //Prints out Capital Y, it works!  
+            char xChar = 'X';
+            Console.WriteLine(++xChar);
+
+        }
+        static void playWithStrings()
+        {
+            char charInString;
+            string aString = "Some rando string";
+            //get the 3rd character in a string
+            charInString = aString[2];
+            Console.WriteLine(charInString);
         }
     }
 }
