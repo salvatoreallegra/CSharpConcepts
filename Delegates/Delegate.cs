6c666ec43a2c36b180091d85c0eb84ce3b94d015
salallegra <salvatore.allegra@smoothstack.com>
2022-10-03 06:33:02 -0700

    Complete Introductory Delegate Example

--- Delegates/Delegate.cs
+++ Delegates/Delegate.cs
@@ -3,28 +3,41 @@
     internal class Delegate
     {
         //Declare a delegate the returns a boolean and takes a number
-        //
+        //a delegate is actually a reference type, like a class, interface or record 
         public delegate bool NumberPredicate(int number);
         static void Main(string[] args)
         {
             int[] numbers = {1,2,3,4,5,6,7,8,9,10};
 
-            NumberPredicate evenPredicate = isEven;
+            //Assign the IsEven function to the variable of type Number Predicate
+            //*Note that we can do this since the IsEven method has the same signature
+            //and return type
+            NumberPredicate evenPredicate = IsEven;
 
             //Call evenpredicate
             Console.WriteLine($"Calling Even Predicate: {evenPredicate(5)}");
+
             List<int> evenNumbers = FilterArray(numbers, evenPredicate);
             
             //Display the list
             DisplayList("Even Numbers",evenNumbers);
+
+            List<int> oddNumbers = FilterArray(numbers, IsOdd);
+            DisplayList("Odd Numbers", oddNumbers);
+
+            List<int> numbersGreaterThan5 = FilterArray(numbers, IsOver5);
+            DisplayList("Numbers > than 5", numbersGreaterThan5);
+
             
             
         }
 
         // function that will return true or false if the number is even or odd.
-        private static bool isEven(int number) => number % 2 == 0;
+        private static bool IsEven(int number) => number % 2 == 0;
 
-        private static bool isOdd(int number) => number % 2 == 1;
+        private static bool IsOdd(int number) => number % 2 == 1;
+
+        private static bool IsOver5(int number) => number > 5;
 
         //Filter a an array with the evenPredicate above
         private static List<int> FilterArray(int[] intArray, NumberPredicate predicate)
@@ -45,7 +58,7 @@
             Console.Write(description);
             foreach(var number in list)
             {
-                Console.Write($"{number} ");
+                Console.Write($" {number} ");
             }
             Console.WriteLine();
         }
